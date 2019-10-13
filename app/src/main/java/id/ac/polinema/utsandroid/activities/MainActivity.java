ef2edabece536b66196c8f45396f6248fd31d213
Moh. Khoirul Arifin <karifin998@gmail.com>
2019-10-13 10:45:35 +0700

    Membuat Activity, Slide, upload Gambar

---
+++ app/src/main/java/id/ac/polinema/utsandroid/activities/MainActivity.java
@@ -0,0 +1,16 @@
+package id.ac.polinema.utsandroid.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import id.ac.polinema.utsandroid.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
